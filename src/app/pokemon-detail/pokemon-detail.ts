bc66fa3f7b346159e91a28feed592661e0a89a61
batophobia <godrox3@yahoo.com>
2026-09-10 13:04:59 -0500

    Update detail component

--- src/app/pokemon-detail/pokemon-detail.ts
+++ src/app/pokemon-detail/pokemon-detail.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { GameService } from '../services/game';
 import { PokemonService } from '../services/pokemon';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pokemon-detail.html',
   styleUrl: './pokemon-detail.css'
 })
 export class PokemonDetail {
   private route = inject(ActivatedRoute);
   private pokemonService = inject(PokemonService);
+  readonly game = inject(GameService);
 
-  readonly pokemon = this.pokemonService.getById(
-    Number(this.route.snapshot.paramMap.get('id'))
+  readonly pokemonId = Number(this.route.snapshot.paramMap.get('id'));
+
+  readonly species = this.pokemonService.getById(this.pokemonId);
+
+  readonly owned = computed(() =>
+    this.game.ownedPokemon().find(
+      pokemon => pokemon.speciesId === this.pokemonId
+    )
   );
 }
